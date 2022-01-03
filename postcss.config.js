f212e2b18e79e5f3490fb93091653b278b9a12a3
Kevin Lloyd <kevin@kevinlloyd.net>
2022-01-03 12:59:26 -0600

    purgecss: specific javascript files

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: ['./**/*.html', './**/*.js'],
+    content: ['./**/*.html', './static/js/flickity.min.js', './static/js/parallax.js', './static/js/scripts.js'],
     //safelist: ['flickity-slider', 'slider', 'slides', 'slide', 'is-selected', 'flickity-prev-next-button', 'flickity-page-dots', 'dot', 'flickity-enabled', 'is-draggable', 'flickity-viewport', 'flickity-slider' ]
 });
 
