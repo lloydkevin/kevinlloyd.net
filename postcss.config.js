3ee93c3f1efef290f7be32fd02466893a9360f03
Kevin Lloyd <kevin@kevinlloyd.net>
2022-01-04 00:49:31 -0600

    concat js

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: ['./**/*.html', './static/js/flickity.min.js', './static/js/parallax.js', './static/js/scripts.js'],
+    content: ['./**/*.html', './assets/js/flickity.min.js', './assets/js/parallax.js', './assets/js/scripts.js'],
     //safelist: ['flickity-slider', 'slider', 'slides', 'slide', 'is-selected', 'flickity-prev-next-button', 'flickity-page-dots', 'dot', 'flickity-enabled', 'is-draggable', 'flickity-viewport', 'flickity-slider' ]
 });
 
