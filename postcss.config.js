0940ce04b2e34e3831780c5c691470561f0bbd83
Kevin Lloyd <kevin@kevinlloyd.net>
2022-01-04 01:41:30 -0600

    Not sure why purge css is different deployed, vs on local

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
     content: ['./**/*.html', './assets/js/flickity.min.js', './assets/js/parallax.js', './assets/js/scripts.js'],
     //safelist: ['flickity-slider', 'slider', 'slides', 'slide', 'is-selected', 'flickity-prev-next-button', 'flickity-page-dots', 'dot', 'flickity-enabled', 'is-draggable', 'flickity-viewport', 'flickity-slider' ]
+    safelist: ['pre', 'figure', 'highlight']
 });
 
 module.exports = {
