f42664fa2cb92c5f345a7f4211e7726ef8196a5c
Kevin Lloyd <kevin@kevinlloyd.net>
2022-01-04 01:55:58 -0600

    Combining multiple options for purge css

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,16 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: ['./**/*.html', './assets/js/flickity.min.js', './assets/js/parallax.js', './assets/js/scripts.js'],
+    content: ['./hugo_stats.json', './**/*.html', './assets/js/flickity.min.js', './assets/js/parallax.js', './assets/js/scripts.js'],
     //safelist: ['flickity-slider', 'slider', 'slides', 'slide', 'is-selected', 'flickity-prev-next-button', 'flickity-page-dots', 'dot', 'flickity-enabled', 'is-draggable', 'flickity-viewport', 'flickity-slider' ]
-    safelist: ['pre', 'figure', 'highlight']
+    // safelist: ['pre', 'figure', 'highlight'],
+    extractors: [
+        {
+          extractor: content => {
+            let els = JSON.parse(content).htmlElements;
+            return els.tags.concat(els.classes, els.ids);
+            },
+          extensions: ['json']
+        }
+      ]
 });
 
 module.exports = {
